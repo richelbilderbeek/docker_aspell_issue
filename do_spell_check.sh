694434d7c90ddcd791d76cf5054f298f0e3470f5
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-07 09:32:29 +0200

    Reproduce bug

--- do_spell_check.sh
+++ do_spell_check.sh
@@ -8,8 +8,16 @@
 #
 # Solution adapted from https://tex.stackexchange.com/a/19
 
-echo "File without spelling errors"
+echo "File without spelling errors, interactively"
 aspell -c --mode=tex correct.tex --lang=en_GB
 
-echo "File with spelling errors"
+echo "File with spelling errors, interactively"
 aspell -c --mode=tex incorrect.tex --lang=en_GB
+
+echo "File without spelling errors"
+cat correct.tex | aspell pipe --mode=tex --lang=en_GB
+
+echo "File with spelling errors"
+cat incorrect.tex | aspell pipe --mode=tex --lang=en_GB
+
+
