ff3644f04e04174fde686dab7c5c0c27532a6036
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-07 11:32:59 +0200

    EOD

--- do_spell_check_docker.sh
+++ do_spell_check_docker.sh
@@ -11,8 +11,8 @@
 echo "File without spelling errors, interactive"
 sudo docker run starefossen/aspell -c --mode=tex correct.tex --lang=en_GB
 
-echo "File with spelling errors, interactive"
-sudo docker run starefossen/aspell -c --mode=tex incorrect.tex --lang=en_GB
+# echo "File with spelling errors, interactive"
+# sudo docker run starefossen/aspell -c --mode=tex incorrect.tex --lang=en_GB
 
 echo "File without spelling errors"
 cat correct.tex | sudo docker run starefossen/aspell pipe --mode=tex --lang=en_GB
