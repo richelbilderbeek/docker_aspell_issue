5aa1d3699259b905cbb647f83c6186f2d66a0665
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-11 08:33:42 +0200

    Can cat to Docker

--- do_spell_check_docker.sh
+++ do_spell_check_docker.sh
@@ -8,11 +8,19 @@
 #
 # Solution adapted from https://tex.stackexchange.com/a/19
 
-#echo "File without spelling errors, interactive"
-#sudo docker run starefossen/aspell -c --mode=tex correct.tex --lang=en_GB
+# echo "File without spelling errors, interactive"
+# sudo docker run -i --volume $PWD/correct.tex:/correct.tex starefossen/aspell -c --mode=tex correct.tex --lang=en_GB
+#
+# Fails with error:
+#
+#   Error: Stdin not a terminal.
 
 # echo "File with spelling errors, interactive"
-# sudo docker run starefossen/aspell -c --mode=tex incorrect.tex --lang=en_GB
+# sudo docker run -i --volume $PWD/incorrect.tex:/incorrect.tex starefossen/aspell -c --mode=tex incorrect.tex --lang=en_GB
+#
+# Fails with error:
+#
+#  Error: Stdin not a terminal.
 
 echo "File without spelling errors"
 cat correct.tex | sudo docker run -i starefossen/aspell --pipe --mode=tex --lang=en_GB
