e42e8c143428375e96db79b4c19768d4d567591e
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-11 08:30:28 +0200

    Can run aspell from Docker

--- do_spell_check_docker.sh
+++ do_spell_check_docker.sh
@@ -8,14 +8,14 @@
 #
 # Solution adapted from https://tex.stackexchange.com/a/19
 
-echo "File without spelling errors, interactive"
-sudo docker run starefossen/aspell -c --mode=tex correct.tex --lang=en_GB
+#echo "File without spelling errors, interactive"
+#sudo docker run starefossen/aspell -c --mode=tex correct.tex --lang=en_GB
 
 # echo "File with spelling errors, interactive"
 # sudo docker run starefossen/aspell -c --mode=tex incorrect.tex --lang=en_GB
 
 echo "File without spelling errors"
-cat correct.tex | sudo docker run starefossen/aspell pipe --mode=tex --lang=en_GB
+cat correct.tex | sudo docker run -i starefossen/aspell --pipe --mode=tex --lang=en_GB
 
 echo "File with spelling errors"
-cat incorrect.tex | sudo docker run starefossen/aspell pipe --mode=tex --lang=en_GB
+cat incorrect.tex | sudo docker run -i starefossen/aspell --pipe --mode=tex --lang=en_GB
